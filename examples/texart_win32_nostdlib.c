0fe34038f962f48005864d8dd986ccb98df1c9f0
nickscha <nick@scharnbeck.de>
2025-09-20 17:22:00 +0200

    [VERSION] bump to v0.2

--- examples/texart_win32_nostdlib.c
+++ examples/texart_win32_nostdlib.c
@@ -1,4 +1,4 @@
-/* texart.h - v0.1 - public domain data structures - nickscha 2025
+/* texart.h - v0.2 - public domain data structures - nickscha 2025
 
 A C89 standard compliant, single header, nostdlib (no C Standard Library) Text Art Generator (TEXART).
 
